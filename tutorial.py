d00a4d86f0626ee433b875267335b79306ebc211
wonth93 <wongth93@gmail.com>
2023-05-30 22:30:51 -0700

    add gravity

--- tutorial.py
+++ tutorial.py
@@ -20,6 +20,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 # Player info and movement
 class Player(pygame.sprite.Sprite):
   COLOR = (255, 0, 0)
+  GRAVITY = 1
 
   def __init__(self, x, y, width, height):
       self.rect = pygame.Rect(x, y, width, height)
@@ -28,6 +29,7 @@ class Player(pygame.sprite.Sprite):
       self.mask = None
       self.direction = "left"
       self.animation_count = 0
+      self.fall_count = 0
 
   def move(self, dx, dy):
     self.rect.x += dx
@@ -46,7 +48,10 @@ class Player(pygame.sprite.Sprite):
       self.animation_count = 0
   
   def loop(self, fps):
+    self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY) # setting the gravity
     self.move(self.x_vel, self.y_vel)
+
+    self.fall_count += 1
 
   def draw(self, win):
     pygame.draw.rect(win, self.COLOR, self.rect)
