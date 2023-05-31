920723e9605b6a228665fa38c8b8877e83a14788
wonth93 <wongth93@gmail.com>
2023-05-30 23:17:48 -0700

    add character with movement

--- tutorial.py
+++ tutorial.py
@@ -17,10 +17,39 @@ FPS = 60 # frame per second
 PLAYER_VEL = 5 # player velocity moving around the screen
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# flip the png from facing right to left
+def flip(sprites):
+  return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+  path = join("assets", dir1, dir2)
+  images = [f for f in listdir(path) if isfile(join(path, f))]
+
+  all_sprites = {}
+
+  for image in images:
+    sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+    sprites = []
+    for i in range(sprite_sheet.get_width() // width):
+      surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+      rect = pygame.Rect(i * width, 0, width, height)
+      surface.blit(sprite_sheet, (0, 0), rect)
+      sprites.append(pygame.transform.scale2x(surface))
+    
+    if direction:
+      all_sprites[image.replace(".png", "") + "_right"] = sprites
+      all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+    else:
+      all_sprites[image.replace(".png", "")] = sprites
+  
+  return all_sprites
+
 # Player info and movement
 class Player(pygame.sprite.Sprite):
   COLOR = (255, 0, 0)
   GRAVITY = 1
+  SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
 
   def __init__(self, x, y, width, height):
       self.rect = pygame.Rect(x, y, width, height)
@@ -48,13 +77,15 @@ class Player(pygame.sprite.Sprite):
       self.animation_count = 0
   
   def loop(self, fps):
-    self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY) # setting the gravity
+    # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY) # setting the gravity
     self.move(self.x_vel, self.y_vel)
 
     self.fall_count += 1
 
   def draw(self, win):
-    pygame.draw.rect(win, self.COLOR, self.rect)
+    # accessing the idle png
+    self.sprite = self.SPRITES["idle_" + self.direction][0]
+    win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 # Function to create the game background
 def get_background(name):
