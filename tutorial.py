41402c3be38aea810681b961c5084b7dc3e242f7
wonth93 <wongth93@gmail.com>
2023-06-05 00:16:30 -0700

    add hit trap effect

--- tutorial.py
+++ tutorial.py
@@ -70,6 +70,8 @@ class Player(pygame.sprite.Sprite):
       self.animation_count = 0
       self.fall_count = 0
       self.jump_count = 0
+      self.hit = False
+      self.hit_count = 0
 
   def jump(self):
     self.y_vel = -self.GRAVITY * 8
@@ -81,6 +83,10 @@ class Player(pygame.sprite.Sprite):
   def move(self, dx, dy):
     self.rect.x += dx
     self.rect.y += dy
+
+  def make_hit(self):
+    self.hit = True
+    self.hit_count = 0
 
   def move_left(self, vel):
     self.x_vel = -vel
@@ -99,6 +105,11 @@ class Player(pygame.sprite.Sprite):
     self.move(self.x_vel, self.y_vel)
 
     self.fall_count += 1
+    if self.hit:
+      self.hit_count += 1
+    if self.hit_count > fps:
+      self.hit = False
+      self.hit_count = 0
     self.update_sprite()
 
   def landed(self):
@@ -112,6 +123,8 @@ class Player(pygame.sprite.Sprite):
 
   def update_sprite(self):
     sprite_sheet = "idle"
+    if self.hit:
+      sprite_sheet = "hit"
     if self.x_vel != 0:
       sprite_sheet = "run"
     if self.y_vel < 0:
@@ -218,7 +231,7 @@ def handle_vertical_collision(player, objects, dy):
       elif dy < 0:
         player.rect.top = obj.rect.bottom
         player.hit_head()
-    collided_objects.append(obj)
+      collided_objects.append(obj)
   return collided_objects
 
 def collide(player, objects, dx):
@@ -250,7 +263,11 @@ def handle_move(player, objects):
   
   
 
-  handle_vertical_collision(player, objects, player.y_vel)
+  vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
+  to_check = [collide_left, collide_right, *vertical_collide]
+  for obj in to_check:
+    if obj and obj.name == "fire":
+      player.make_hit()
 
 # Main function - what we run to start the game
 def main(window):
